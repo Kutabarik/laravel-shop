0885a0870a0e45e63b6144811ba6ee9afa8b725f
kutabarik <malay.misha@gmail.com>
2025-04-20 12:18:26 +0300

    feat: add Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,27 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Category extends Model
+class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'slug',
         'title',
+        'thumbnail',
+        'price',
+        'brand_id',
     ];
 
     protected static function boot(): void
     {
         parent::boot();
 
-        static::creating(function (Category $category) {
-            $category->slug = $category->slug ?? str($category->title)->slug();
+        static::creating(function (Product $product) {
+            $product->slug = $product->slug ?? str($product->title)->slug();
         });
     }
 
-    public function products(): BelongsToMany
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class);
     }
