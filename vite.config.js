481562a065d2469e0dae4234f8532d0d3b50b545
kutabarik <malay.misha@gmail.com>
2025-05-04 10:44:03 +0300

    feat: add login form

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
                 'resources/sass/main.sass',
+                'resources/css/app.css',
                 'resources/js/app.js'
             ],
             refresh: true,
